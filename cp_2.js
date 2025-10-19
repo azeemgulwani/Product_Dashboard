53a84b4fae9474e80b9e0df4c31ade6bd7a48b43
Azeem Gulwani <azeemgulwani@usf.edu>
2025-10-19 01:09:07 -0400

    added displayProducts() function to render first five products dynamically

--- cp_2.js
+++ cp_2.js
@@ -25,3 +25,20 @@ async function fetchProductsAsync() {
     handleError(error);
   }
 }
+
+function displayProducts(products) {
+  var container = document.getElementById("product-container");
+  container.innerHTML = "";
+  for (var i = 0; i < 5; i++) {
+    var p = products[i];
+    var name = p.fields.name;
+    var fixedName = name.charAt(0).toUpperCase() + name.slice(1);
+    var productCard = document.createElement("div");
+    productCard.className = "product-card";
+    productCard.innerHTML =
+      '<img src="' + p.fields.image[0].url + '" alt="' + fixedName + '">' +
+      '<div class="name">' + fixedName + '</div>' +
+      '<div class="price">$' + (p.fields.price / 100).toFixed(2) + '</div>';
+    container.appendChild(productCard);
+  }
+}
